89175b2e311aa52b4c6109ff268a1a4f3b0fb324
SftAlcoholic <victmanuel04.06@gmail.com>
2023-05-08 19:22:30 -0500

    send the generated to the Wordpress API

--- textai.py
+++ textai.py
@@ -1,6 +1,8 @@
 import openai
 from fastapi import FastAPI
 from pydantic import BaseModel
+import requests
+
 
 # Initialize the FastAPI app
 app = FastAPI()
@@ -12,8 +14,6 @@ class TextInput(BaseModel):
 # Initialize the OpenAI API client with your API key
 from config import OPENAI_API_KEY
 openai.api_key = OPENAI_API_KEY
-
-import openai
 
 # Define a function to generate a title for a Wordpress page
 def generate_title(text, redaction_type, audience, industry, language):
@@ -40,12 +40,12 @@ def generate_title(text, redaction_type, audience, industry, language):
 
 
 # Define a function to generate an introduction to the topic
-def generate_intro(text, redaction_type, audience, industry, language, lastPromptRes):
+def generate_intro(text, redaction_type, audience, industry, language, previous_prompt_result):
     # Define the agent profile and prompt for the introduction
     agent_profile = "an opinion journalist, economy expert, and writer"
     context_params = f"working on a {redaction_type} wordpress article targeting a {audience} audience in the {industry} industry, written in {language} (use this params as metadata u dont need to write it in the text, just use it as context)"
     task = f"Write an introduction for a page about {text}"
-    lastPromptContext = f"The title of the Wordpress page is:{lastPromptRes} (use this params as metadata u dont need to write it in the text, just use it as context)"
+    lastPromptContext = f"The title of the Wordpress page is:{previous_prompt_result} (use this params as metadata u dont need to write it in the text, just use it as context)"
     prompt = f"You are {agent_profile}, {context_params}. {task}. {lastPromptContext}"
     try:
         # Use OpenAI's API to generate an introduction based on the prompt
@@ -64,12 +64,12 @@ def generate_intro(text, redaction_type, audience, industry, language, lastPromp
         raise ValueError(str(e))
     
 # Define a function to generate a list of key points
-def generate_points(text, redaction_type, audience, industry, language, lastPromptRes):
+def generate_points(text, redaction_type, audience, industry, language, previous_prompt_result):
     # Define the agent profile and prompt for the introduction
     agent_profile = "an opinion journalist, economy expert, and writer"
     context_params = f"working on a {redaction_type} article targeting a {audience} audience in the {industry} industry, written in {language} (use this params as metadata u dont need to write it in the text, just use it as context)"
     task = f"Write a List three key points about {text}"
-    lastPromptContext = f"The intro of the Wordpress page is:{lastPromptRes} (use this params as metadata u dont need to write it in the text, just use it as context)"
+    lastPromptContext = f"The intro of the Wordpress page is:{previous_prompt_result} (use this params as metadata u dont need to write it in the text, just use it as context)"
     prompt = f"You are {agent_profile}, {context_params}. {task}. {lastPromptContext}"    
     try:
         # Use OpenAI's API to generate an introduction based on the prompt
@@ -88,12 +88,12 @@ def generate_points(text, redaction_type, audience, industry, language, lastProm
         raise ValueError(str(e))
 
 # Define a function to generate a summary of the key points
-def generate_conclusions(text, redaction_type, audience, industry, language, lastPromptRes):
+def generate_conclusions(text, redaction_type, audience, industry, language, previous_prompt_result):
     # Define the agent profile and prompt for the introduction
     agent_profile = "an opinion journalist, economy expert, and writer"
     context_params = f"working on a {redaction_type} article targeting a {audience} audience in the {industry} industry, written in {language} (use this params as metadata u dont need to write it in the text, just use it as context)"
     task = f"Write some conclusions about {text}"
-    lastPromptContext = f"The key points of the page are:{lastPromptRes} (use this params as metadata u dont need to write it in the text, just use it as context)"
+    lastPromptContext = f"The key points of the page are:{previous_prompt_result} (use this params as metadata u dont need to write it in the text, just use it as context)"
     prompt = f"You are {agent_profile}, {context_params}. {task}. {lastPromptContext}"    
     try:
         response = openai.Completion.create(
@@ -109,6 +109,36 @@ def generate_conclusions(text, redaction_type, audience, industry, language, las
     except Exception as e:
         # If there's an error with the OpenAI API, raise a ValueError with the error message
         raise ValueError(str(e))
+
+
+# Define the URL of the Wordpress API endpoint
+WORDPRESS_API_URL = "http://192.168.0.39/pagab/w/wp-json/wp/v2/pages"
+
+# Define a function to send the generated Wordpress page to the Wordpress API
+def send_to_wordpress(title, content):
+    # Define the request body
+    data = {
+        "title": title,
+        "content": content,
+        "status": "publish"
+    }
+
+    # Define the request headers
+    headers = {
+        "Authorization": "Bearer YOUR_WORDPRESS_API_TOKEN",
+        "Content-Type": "application/json"
+    }
+
+    # Send the HTTP POST request to the Wordpress API
+    response = requests.post(WORDPRESS_API_URL, json=data, headers=headers)
+
+    # Check the response status code
+    if response.status_code == 201:
+        print("Page published successfully!")
+    else:
+        print("Failed to publish page. Response content:")
+        print(response.content)
+
     
 # Define an API endpoint that generates a Wordpress page using GPT-3
 @app.post("/generate")
@@ -117,19 +147,19 @@ def generate_wordpress_page(input_data: TextInput, redaction_type: str, language
         # Generate the content for the Wordpress page
         # Step 1: Generate the title
         title = generate_title(input_data, redaction_type, audience, industry, language)
-        lastPromptRes = title
+        previous_prompt_result = title
         
         # Step 2: Generate an introduction to the topic
-        intro_text = generate_intro(input_data, redaction_type, audience, industry, language, lastPromptRes)
-        lastPromptRes = intro_text
+        intro_text = generate_intro(input_data, redaction_type, audience, industry, language, previous_prompt_result)
+        previous_prompt_result = intro_text
 
         # Step 3: Generate a list of key points
-        points_text = generate_points(input_data, redaction_type, audience, industry, language, lastPromptRes)
-        lastPromptRes = points_text
+        points_text = generate_points(input_data, redaction_type, audience, industry, language, previous_prompt_result)
+        previous_prompt_result = points_text
 
         # Step 4: Generate a summary of the key points
-        summary_text = generate_conclusions(input_data, redaction_type, audience, industry, language, lastPromptRes)
-        lastPromptRes = summary_text
+        summary_text = generate_conclusions(input_data, redaction_type, audience, industry, language, previous_prompt_result)
+        previous_prompt_result = summary_text
 
         # Step 5: Generate the final Wordpress page content
         page_content = f"""
@@ -142,13 +172,10 @@ def generate_wordpress_page(input_data: TextInput, redaction_type: str, language
         """
 
         # Send the page content to the Wordpress API
-        #headers = {"Authorization": f"Bearer {os.environ.get('WORDPRESS_API_KEY')}"}
-        #payload = {"content": page_content}
-        #response = requests.post(f"{os.environ.get('WORDPRESS_WEBSITE_URL')}/wp-json/wp/v2/pages", headers=headers, data=payload)
-        
-        # Return the response from the Wordpress API
+        send_to_wordpress(title, page_content)
+
         #return {"response": response.text}
         return {"page_content": page_content}
-    
+
     except Exception as e:
         return {"error": str(e)}
